ce103596aa8e8a8e2adebc3e281157dcb4d9ac82
999Anastasia999 <anastasiya.kozlova.89.ak@gmail.com>
2022-09-19 20:43:10 +0300

    add задача 3 (5)

---
+++ Задача3/Program.cs
@@ -0,0 +1,11 @@
+﻿//5. Напишите программу вычисления значения функции возведения числа в квадрат
+int Power(int x) 
+{
+    return x * x;
+}
+
+Console.WriteLine("Ведите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(Power(number));
+
+
