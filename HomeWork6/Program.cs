c240d0bb8ef2587959cbcfce35614a3c1e7cadc0
999Anastasia999 <anastasiya.kozlova.89.ak@gmail.com>
2022-09-19 22:19:07 +0300

    add Д/З Задача №6

---
+++ HomeWork6/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задача 6: Напишите программу, которая на вход принимает число и выдаёт, 
+            //является ли число четным (делится ли оно на два без остатка).
+            Console.Write("Введите число1: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            string Answer = "";           
+            if (num % 2 == 0)
+            {
+                Answer = "четное";
+            }
+            else
+            {
+                Answer = "нечетное";
+            }
+
+            Console.Write("Ответ: введенное число " + Answer);  
+        }
+
+        #region Fill and Print
+        
+    }
+}
+#endregion
