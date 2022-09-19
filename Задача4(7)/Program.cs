ee227c655c192e3e18d17dd9a524a2bdc5a7b557
999Anastasia999 <anastasiya.kozlova.89.ak@gmail.com>
2022-09-19 21:09:41 +0300

    add задача 4 (7)

---
+++ Задача4(7)/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Напишите программу, которая на вход принимает одно число (N), 
+            // а на выходе показвает все целые числа в промежутке от -N до N/
+            Console.Write("Введите число: ");
+            int num = Convert.ToInt32(Console.ReadLine()); 
+            num = Math.Abs(num);
+            int num2 = -num;
+            while (num2 <= num)
+            {
+                Console.Write(num2 + ",");
+                num2 = num2 + 1;
+            }
+            //Console.WriteLine("\b\b\b\b");
+            Console.WriteLine();
+        }
+
+        #region Fill and Print
+        
+    }
+}
+#endregion
